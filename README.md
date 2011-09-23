2ab3e6e17c0ee1cc4d5f5a465318ea0daaa24f8d
Dmitry Gusev <dmitry.gusev@gmail.com>
2011-09-23 14:42:07 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<a href="javascript:var%20b=document.body;if(b&&!document.xmlVersion){void(z=document.createElement('script'));void(z.src='http://github.com/anjlab/bookmarklets/free-lance-projects-previews-bookmarklet.js');void(b.appendChild(z));}else{}">
+Free-Lance.ru: Projects Previews
+</a>
---
+++ free-lance-projects-previews-bookmarklet.js
@@ -0,0 +1,54 @@
+(function() {
+	if(typeof jQuery=='undefined') {
+		var s = document.createElement('script');
+		s.setAttribute('src','http://jquery.com/src/jquery-latest.js');
+		document.getElementsByTagName('head')[0].appendChild(s);
+	}
+
+	setTimeout(function(){	// wait until jQuery loaded
+
+		var projects = new Array();
+
+		jQuery(document).ready(function(){
+			jQuery(".even").each(function(){
+				var $link = jQuery(this).find("a");
+				var $div = $link.after("<div href='" + $link.attr("href") + "'>Loading...</div>");
+				projects.push($link.next());
+			});
+		});
+
+		var index = 0;
+
+		var intervalID = setInterval(function(){
+			if (index >= projects.length) {
+				clearInterval(intervalID);
+				return;
+			}
+
+			var $div = projects[index];
+			$div.load($div.attr("href") + " #proj_pict", function(response, status, xhr) {
+				if (status == "success") {
+					var $img = $div.find("img");
+
+					var w = $img.attr("width");
+					var h = $img.attr("height");
+
+					var maxSize = 200;
+
+					if (w > h) {
+						h = (maxSize / w) * h;
+						w = maxSize;
+					} else {
+						w = (maxSize / h) * w;
+						h = maxSize;
+					}
+
+					$img.attr("width", w);
+					$img.attr("height", h);
+				}
+			});
+
+			index++;
+		}, 500);		
+	}, 100);
+})();
